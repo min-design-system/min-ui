0c3031c48d9ad464a7abd7e0e978dd63e581edf2
김종혁 <kimjhs@kakao.com>
2024-08-12 18:20:56 +0900

    fix: 누락된 prop 부여

--- components/Badge/Badge.stories.tsx
+++ components/Badge/Badge.stories.tsx
@@ -16,6 +16,8 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: 'Badge'
+    children: 'Badge',
+    size: 'medium',
+    color: 'default'
   }
 };
--- components/Badge/Badge.typing.ts
+++ components/Badge/Badge.typing.ts
@@ -4,5 +4,8 @@ import { BrandColorKey, SecondaryColorKey } from '@core/theme/colors/typing';
 
 export interface BadgeProps extends HTMLAttributes<HTMLSpanElement> {
   size?: 'medium' | 'small';
-  color?: Extract<BrandColorKey, 'primary' | 'danger'> | Extract<SecondaryColorKey, 'inverse'>;
+  color?:
+    | 'default'
+    | Extract<BrandColorKey, 'primary' | 'danger'>
+    | Extract<SecondaryColorKey, 'inverse'>;
 }
